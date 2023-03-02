1ad1f3950875b6925b89088bc2d0002d481cdf31
fredjacobs <thefredjacobs@gmail.com>
2023-03-02 18:08:06 +0200

    Add Keyup event

--- script.js
+++ script.js
@@ -7,9 +7,12 @@ let firstNum = '';
 let secondNum = '';
 let operator = '';
 let answer = '';
+let keyboardInput = '';
 
 let isAddingToFirstStr = false;
 let isAddingToSecondStr = false;
+let isKeyNumber = false;
+let isKeyOperator = false;
 
 function operate(operator, num1, num2){
     switch (operator) {
@@ -42,14 +45,67 @@ function operate(operator, num1, num2){
 
 }
 
-function getInput(inputValue){
-    let input = inputValue.target.innerText;
+function getKeyboardInput(input){
+        
+    if(input === 'Delete'){
+        clearFields();
+    }
 
+    if (isKeyNumber){
+        if (operator == ''){
+            isAddingToFirstStr = true;
+            isAddingToSecondStr = false;
+            firstNum += input;
+            display.innerText = firstNum;
+            
+        }else if (firstNum != '') {
+            isAddingToFirstStr = false;
+            isAddingToSecondStr = true;
+            display.innerText = '';
+            secondNum += input;
+            display.innerText = secondNum;
+        } 
+        
+        
+    }
+    if (isKeyOperator){
+        console.log(`Operator: ${input}`);
+        
+        if(firstNum != '' && secondNum != '' && operator != ''){
+            
+            answer = operate(operator, +firstNum, +secondNum);
+            display.innerText = answer;
+            firstNum = answer;
+            secondNum = '';
+        }
+        operator = input;
+
+        
+        
+    }
+
+    if(input === 'Enter')
+    {
+        if(operator == '' || firstNum == '' || secondNum == ''){
+            return;
+        }
+
+        display.innerText = operate(operator, +firstNum, +secondNum);
+    }
+
+    if(input === 'Backspace'){deleteChar();}
+      
+}
+
+function getInput(inputValue){
+        
+    let = input = inputValue.target.innerText;
+    
     if(input === 'C'){
         clearFields();
     }
 
-    if (inputValue.target.classList.contains('num-field')){
+    if (inputValue.target.classList.contains('num-field') ){
         if (operator == ''){
             isAddingToFirstStr = true;
             isAddingToSecondStr = false;
@@ -78,7 +134,6 @@ function getInput(inputValue){
         
     }
 
-    
     if(input === '=')
     {
         if(operator == '' || firstNum == '' || secondNum == ''){
@@ -86,6 +141,9 @@ function getInput(inputValue){
         }
 
         display.innerText = operate(operator, +firstNum, +secondNum);
+        display.innerText = answer;
+        firstNum = answer;
+        secondNum = '';
     }
       
 }
@@ -95,7 +153,7 @@ function clearFields(){
         secondNum = '';
         operator = '';
         answer = '';
-        display.innerText = '';
+        display.innerText = '0';
 }
 
 function deleteChar(){
@@ -110,5 +168,35 @@ function deleteChar(){
     
 }
 
+function sortKeyboardInput(keyInput){
+
+    /* console.log(keyInput.key); */
+    
+    let input = keyInput.key;
+
+    let numbers = ['0','1','2','3','4','5','6','7','8','9'];
+    let operators = ['/','*','+','-'];
+
+    if (numbers.includes(input)){
+        isKeyNumber = true;
+        isKeyOperator = false;
+        getKeyboardInput(input);
+        
+    }else if(operators.includes(input)){
+        isKeyOperator = true;
+        isKeyNumber = false;
+        getKeyboardInput(input);
+    }else{
+        isKeyOperator = false;
+        isKeyNumber = false;
+        getKeyboardInput(input);
+        console.log(input);
+    }
+
+
+}
+
 delBtn.addEventListener('click', deleteChar);
 buttons.forEach(el => el.addEventListener('click', getInput));
+
+document.addEventListener('keyup', sortKeyboardInput);
